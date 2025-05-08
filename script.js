0d1b698b92a19441c557f9743dc9e3ed57de9774
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-08 14:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
-    //Write your code here
   
-} 
+    const radiusInput = document.getElementById("radius").value;
+    const radius = parseFloat(radiusInput);
+  
+
+    if (isNaN(radius) || radius < 0) {
+      document.getElementById("volume").value = "NaN";
+      return false;
+    }
+  
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    document.getElementById("volume").value = volume.toFixed(4);
+    return false; 
+  }
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
